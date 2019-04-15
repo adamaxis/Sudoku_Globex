162294bd85b00c8057be13ecd3dd8fad37dab517
Michelle Taylor <mmtaylor3@dmacc.edu>
2019-04-14 22:48:20 -0500

    Added input validation for user menu.

--- Sudoku_Globex/Sudoku_Globex.cpp
+++ Sudoku_Globex/Sudoku_Globex.cpp
@@ -9,7 +9,7 @@ void mainMenu();
 void createBoard();
 void printBoard();
 int board[9][9];
-int columnChoice, rowChoice;
+int response, columnChoice, rowChoice;
 using namespace std;
 
 int main()
@@ -18,14 +18,37 @@ int main()
 	createBoard();
 	printBoard();
 	mainMenu();
+
+	return 0;
 }
 
 void mainMenu()
 {
-	cout << "\nColumn: ";
-	cin >> columnChoice;
-	cout << "Row: ";
-	cin >> rowChoice;
+	bool columnQuest = true, rowQuest = true;
+
+	while (columnQuest != false) {
+		cout << "\n\t\tColumn: ";
+		cin >> response;
+		if (response > 9 || response < 1) {
+			cout << "   Please enter a number between 1 and 9.";
+		}
+		else {
+			columnChoice = response;
+			columnQuest = false;
+		}
+	}
+
+	while (rowQuest != false) {
+		cout << "\n\t\t  Row: ";
+		cin >> response;
+		if (response > 9 || response < 1) {
+			cout << "   Please enter a number between 1 and 9.";
+		}
+		else {
+			rowChoice = response;
+			rowQuest = false;
+		}
+	}
 }
 
 void createBoard()
@@ -41,10 +64,11 @@ void printBoard()
 {
 
 	for (int i = 0; i <= 8; i++) {
-		cout << endl << endl;
+		cout << endl << "     ";
 		for (int j = 0; j <= 8; j++) {
-			cout << "   " << board[i][j] << " ";
+			cout << "| " << board[i][j] << " ";
 		}
+		cout << "|" << endl;
 	}
 
 	cout << endl;
